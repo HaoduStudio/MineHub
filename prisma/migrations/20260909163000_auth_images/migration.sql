39b5026d320ba9659530707bd52a9fb5dfeed978
Hikaru <imhikaru@proton.me>
2026-09-09 19:29:03 +0800

    chore: drop section comment from auth images migration

--- prisma/migrations/20260909163000_auth_images/migration.sql
+++ prisma/migrations/20260909163000_auth_images/migration.sql
@@ -1,4 +1,3 @@
--- AlterTable
 ALTER TABLE "SiteSettings"
 ADD COLUMN     "authImageLight" TEXT NOT NULL DEFAULT 'https://upload.wikimedia.org/wikipedia/commons/c/ca/Minecraft_-_Taiga.jpg',
 ADD COLUMN     "authImageDark" TEXT NOT NULL DEFAULT 'https://upload.wikimedia.org/wikipedia/commons/c/cd/Screenshot_from_the_Minecraft_Nether.png',
